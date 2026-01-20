fe099c83622a43759d87339694be87cfaeb8c001
sonnsept-glitch <sonnsept@gmail.com>
2026-01-20 10:19:37 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -20,14 +20,11 @@ download_if_needed
 while true; do
   if is_port_open; then
     echo "[OK] ttyd already listening on port $PORT"
+    sleep 24h
   else
     echo "[WARN] Port $PORT not open → restarting ttyd"
-
-    pkill -f "$BIN" || true
+    pkill -f $BIN" || true
     sleep 5
-
     "$BIN" -p $PORT -m 200 -w ~ --writable bash &
   fi
-
-  sleep 5
 done
